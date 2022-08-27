620f2285c18ad0a3287f5a6c780b8b52512e05ad
Nicoli <nicolidss@gmail.com>
2022-08-27 14:28:38 -0300

    Solução ruim para as exceções.

--- src/Program.java
+++ src/Program.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Program {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) throws ParseException { // desse jeito essa exceção vai ser propagada e não tratada
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Scanner src = new Scanner(System.in);
+
+        System.out.print("Número do quarto:");
+        int quarto= src.nextInt();
+        System.out.print("Data de Entrada:");
+        Date entrada = sdf.parse(src.next());// o parce que exige o tratamento de exceção
+        System.out.print("Data de Saída:");
+        Date saida = sdf.parse(src.next());
+        Reserva reserva = new Reserva(quarto,entrada,saida);
+
+        if(saida.after(entrada)){
+            System.out.println("Reserva: "+ reserva);}
+        else
+            System.out.println("Data de saída incorreta");
+
+
+        System.out.println();
+        System.out.println("Digite os dados para atualizar a reserva");
+        System.out.print("Nova data de Entrada:");
+        entrada = sdf.parse(src.next());// o parce que exige o tratamento de exceção
+        System.out.print("Nova data de Saída:");
+        saida = sdf.parse(src.next());
+        reserva.atualizarDatas(entrada,saida);
+
+        String erro = reserva.atualizarDatas(entrada,saida);
+        if(erro != null){
+            System.out.println("Erro na reserva:" + erro);
+        }else
+            System.out.println("Reserva atualizada: "+ reserva);
+
+
+    }
+
 }
--- src/Reserva.java
+++ src/Reserva.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class Reserva {
+import javax.xml.crypto.Data;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reserva {
+    private Integer numQuarto;
+    private Date entrada;
+    private Date saida;
+
+    public Reserva() {
+    }
+
+    public Reserva(Integer numQuarto, Date entrada, Date saida) {
+        this.numQuarto = numQuarto;
+        this.entrada = entrada;
+        this.saida = saida;
+    }
+
+    public Integer getNumQuarto() {
+        return numQuarto;
+    }
+
+    public void setNumQuarto(Integer numQuarto) {
+        this.numQuarto = numQuarto;
+    }
+
+    public Date getEntrada() {
+        return entrada;
+    }
+
+    public Date getSaida() {
+        return saida;
+    }
+    //Método que cálcula a diferença em milisegundos e depois converte para dias
+    //
+    public long duracao(){
+        long diferenca = saida.getTime() - entrada.getTime();
+        return  TimeUnit.DAYS.convert(diferenca,TimeUnit.MILLISECONDS);
+    }
+
+    public String atualizarDatas(Date entrada,Date saida){
+        Date agora=new Date(); // a reserva só pode ser alterada para datas futuras
+        if(entrada.before(agora)){
+           return "Datas de atualização devem ser futuras.";}
+        if (!saida.after(entrada)){
+            return "Data de saída incorreta, deve ser escolhida uma data posterior a de entrada.";}
+
+        this.entrada = entrada;
+        this.saida = saida;
+        return null;
+    }
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    @Override
+    public String toString() {
+        return "Reserva{" +
+                "Numero do Quarto= " + numQuarto +
+                ", data de entrada=" + sdf.format(entrada) +
+                ", data de saída=" + sdf.format(saida) +" "+
+                duracao()+ " noites"+
+                '}';
+    }
+
+
 }
